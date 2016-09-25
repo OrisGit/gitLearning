c0b114bfbd1315e96e02c2946656d23789c65b6f
Oris <chief.oris@gmail.com>
2016-09-25 14:03:47 +0400

    text

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Oris on 25.09.2016.
+ */
+public class Main {
+    public static void Main(String[] args)
+    {
+        System.out.print("GitLesson");
+        System.out.print("Status");
+        System.out.print("Status");
+        System.out.print("Status");
+    }
+}
