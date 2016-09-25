f14c0a819aba6fcbd38330da8900464f95303c50
Oris <chief.oris@gmail.com>
2016-09-25 14:06:09 +0400

    text

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,9 @@ public class Main {
         System.out.print("Status");
         System.out.print("Status");
         System.out.print("Status");
+        System.out.print("Status");
+        System.out.print("Status");
+        System.out.print("Status");
+        System.out.print("Status");
     }
 }
